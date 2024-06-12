a60d68a5ee4960b281abdafa9d1b05d74dc4b0c2
Łukasz Gadzina <l.gadzina@dookpro>
2024-06-12 16:54:01 +0200

    poprawa

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
+import { ChangeEvent, useState } from 'react'
 
 function App() {
   const [name, setName] = useState<string>('Podaj swoje imie')
 
   return (
     <>
-      <input onChange={((e: HTMLInputElement) => setName(e.value))} /><br/>
+      <input onChange={((e: ChangeEvent<HTMLInputElement>) => setName(e.target.value))} /><br/>
 
       {name}
     </>
