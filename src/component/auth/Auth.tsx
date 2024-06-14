e832606a7e0e5367fe6bc9b84e1891e5c3d103dd
Łukasz Gadzina <l.gadzina@dookpro>
2024-06-14 14:07:28 +0200

    router

---
+++ src/component/auth/Auth.tsx
@@ -0,0 +1,9 @@
+import { Outlet } from "react-router"
+
+const Auth = () => {
+    return <>
+        <Outlet />
+    </>
+}
+
+export default Auth;
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import MainRoutes from './config/main_routes.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <MainRoutes />
   </React.StrictMode>,
 )
